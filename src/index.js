fe1e50ee369eced616a21c64b14958d18573cde3
ZhangJiaSong <zhangjiasong@bonc.com.cn>
2022-04-15 16:48:36 +0800

    webpack上手练习

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
-function add(a, b) {
-  return a + b;
+// import "./index.css";
+import img from "./public/微信图片_20220221112959.jpg";
+import "./index.scss";
+const img1 = new Image();
+img1.src = img;
+console.log();
+document.getElementById("imgBox1").appendChild(img1);
+function heapSort(arr) {
+  let len = arr.length;
+
+  for (let i = Math.floor(len / 2) - 1; i >= 0; i--) {
+    heapify(arr, i, len);
+  }
+  for (let j = len - 1; j >= 1; j--) {
+    let temp = arr[j];
+    arr[j] = arr[0];
+    arr[0] = temp;
+    heapify(arr, 0, --len);
+  }
+  return arr;
 }
 
-const sum = add(1, 2);
-module.exports = sum;
+function heapify(arr, i, len) {
+  let l = 2 * i + 1,
+    r = 2 * i + 2,
+    largest = i;
+
+  if (l < len && arr[largest] < arr[l]) {
+    largest = l;
+  }
+  if (r < len && arr[largest] < arr[r]) {
+    largest = r;
+  }
+  if (largest != i) {
+    let temp = arr[i];
+    arr[i] = arr[largest];
+    arr[largest] = temp;
+    heapify(arr, largest, len);
+  }
+}
+console.log(
+  heapSort([91, 60, 96, 13, 35, 65, 46, 65, 10, 30, 20, 31, 77, 81, 22])
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-console.log("环境变量", process.env.NODE_ENV);
+const MinCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+console.log("环境变量", process.env.NODE_ENV, __dirname);
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -12,18 +14,69 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.(s[ac]|c)ss$/i, //匹配所有的scss sass css文件
+        use: [
+          // "style-loader"
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      // {
+      //   test: /\.(jpe?g|png|gif)$/i,
+      //   //使用file-loader解决webpack不认识图片的问题
+      //   use: [
+      //     // "file-loader",
+      //     {
+      //       loader: "url-loader",
+      //       options: {
+      //         name: "[name].[hash:8].[ext]",
+      //         limit: 50 * 1024,
+      //       },
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        type: "asset",
+        generator: {
+          filename: "[name][hash:8][ext]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 50 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        type: "asset",
+        generator: {
+          filename: "[name][hash:8][ext]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024,
+          },
+        },
       },
     ],
   },
   plugins: [
+    //样式文件单独打包
+    new MinCssExtractPlugin({
+      filename: "[name].[hash:8].css",
+    }),
+    //指定打包模板HTML
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    //清理旧文件
     new CleanWebpackPlugin(),
   ],
   devServer: {
+    //静态文件打包
     static: {
       directory: path.join(__dirname, "public"),
     },
