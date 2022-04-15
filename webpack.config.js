3e6679ce1560eebf91aad188a3ffdfb4b90f4af6
ZhangJiaSong <zhangjiasong@bonc.com.cn>
2022-04-15 09:20:46 +0800

    webpack学习

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+console.log("环境变量", process.env.NODE_ENV);
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 8088,
+  },
+};
